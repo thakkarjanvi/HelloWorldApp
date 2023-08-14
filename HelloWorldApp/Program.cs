3d1b7b564e0a80f984fd9ce3341a5222923979fa
Janvi <janvithakkar18122001@example.com>
2023-08-14 23:39:05 +0530

    Add divide method

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -10,13 +10,19 @@
         {
             return num1 * num2;
         }
+        private static int Divide(int num1, int num2)
+        {
+            return num1 * num2;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             int result = Sum(5, 7);
             Console.WriteLine(result);
-            int product = Sum(5, 7);
+            int product = Multiply(5, 7);
             Console.WriteLine(product);
+            int divide = Divide(15, 5);
+            Console.WriteLine(divide);
         }
     }
 }
